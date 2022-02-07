cb21b1826df897aa1da867f73112f7692dea31b4
adrianstrong <93924821+adrianstrong@users.noreply.github.com>
2022-02-07 14:45:01 +0100

    Update Equipo.java

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,35 +1,32 @@
 public class Equipo {
-	private String nombreEquipo;
-	private int ranking;
-
-	public Equipo() {
-	}
-
-	public void setNombreEquipo(String nombreEquipo) {
-		if (nombreEquipo.length() >= 4 && nombreEquipo.length() <= 20) {
-			this.nombreEquipo = nombreEquipo;
-		} else {
-			this.nombreEquipo = null;
-		}
-		
-	}
-
-	public void setRanking(int ranking) {
-
-		if (ranking >= 0 && ranking <= 10) {
-			this.ranking = ranking;
-		} else {
-			this.ranking = -1;
-		}
-	}
-
-	public String getNombreEquipo() {
-		return nombreEquipo;
-
-	}
-
-	public int getRanking() {
-		return ranking;
-
-	}
+    private String nombreEquipo;
+    private int ranking;
+        public void setNombreEquipo(String nombreEquipo) {
+                if (nombreEquipo.length() >= 4 && nombreEquipo.length() <= 20) {
+                    for (int i = 0; i < nombreEquipo.length(); i++) {
+                        if (nombreEquipo.toLowerCase().charAt(i) >= 'a' && nombreEquipo.toLowerCase().charAt(i) <= 'z' || nombreEquipo.charAt(i) == ' ') 
+                        {
+                            this.nombreEquipo = nombreEquipo;
+                        } else {
+                            this.nombreEquipo = null;
+                            i = nombreEquipo.length();
+                        }
+                    }
+                } else {
+                    this.nombreEquipo = null;
+                }
+            }
+    public void setRanking(int ranking) {
+        if (ranking >= 0 && ranking <= 10) {
+            this.ranking = ranking;
+        } else {
+            this.ranking = -1;
+        }
+    }
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+    public int getRanking() {
+        return ranking;
+    }
 }
