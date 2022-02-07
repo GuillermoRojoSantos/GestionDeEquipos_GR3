f278eef7f49969ecd20684c1bd1ebd045703891a
GuillermoRojoSantos <guillermo.s250439@cesurformacion.com>
2022-02-07 09:29:27 +0100

    Added AsignarEquipo.java and AsignarEquipoTest.java cuz idk i'm dumb and made a push before doing an update to my local project

--- src/AsignarEquipo.java
+++ src/AsignarEquipo.java
@@ -2,26 +2,26 @@
 public class AsignarEquipo {
       private Equipo equipo;
       private Jugador jugador;
-      
-      
+
+
       public AsignarEquipo() {
 
       }
 
       public void setEquipo(Equipo equipo) {
-        //code here
+            //code here
       }
 
       public void setJugador(Jugador jugador) {
-  		//code here
+            //code here
       }
 
       public Equipo getEquipo(){
-          return equipo;
+            return equipo;
       }
-      
+
       public Jugador getJugador(){
-         return jugador;
+            return jugador;
       }
-      
+
 }
---
+++ src/AsignarEquipoTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AsignarEquipoTest {
+
+    @Test
+    void TestEquipoNoExiste(){
+        Equipo equipo= null;
+        assertEquals(null,equipo);
+    }
+    /*Tests de quipo correcto dividido en dos pruebas para el nombre y ranking*/
+    @Test
+    void TestNombreEquipoCorrecto(){
+        Equipo equipo = new Equipo();
+        String nombreEquipo= "Real Madrid";
+        equipo.setNombreEquipo(nombreEquipo);
+        equipo.setRanking(4);
+        assertEquals(nombreEquipo, equipo.getNombreEquipo());
+    }
+    @Test
+    void TestRankingEquipoCorrecto() {
+        Equipo equipo = new Equipo();
+        String nombreEquipo = "Real Madrid";
+        int rankingEquipo=4;
+        equipo.setNombreEquipo(nombreEquipo);
+        equipo.setRanking(rankingEquipo);
+        assertEquals(rankingEquipo,equipo.getRanking());
+    }
+
+    @Test
+    void TestNombreEquipoIncorrecto(){
+        Equipo equipo=new Equipo();
+        String nombreEquipo="Rea";
+        int rankingEquipo=4;
+        equipo.setNombreEquipo(nombreEquipo);
+        equipo.setRanking(rankingEquipo);
+        assertEquals(null,equipo.getNombreEquipo());
+    }
+
+    @Test
+    void TestRankingEquipoIncorrecto(){
+        Equipo equipo=new Equipo();
+        String nombreEquipo="Real Madrid";
+        int rankingEquipo=52425;
+        equipo.setNombreEquipo(nombreEquipo);
+        equipo.setRanking(rankingEquipo);
+        assertEquals(0, equipo.getRanking());
+    }
+
+}
