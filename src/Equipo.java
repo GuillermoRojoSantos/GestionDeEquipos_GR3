982f8dc295fcd2718649c499f7ecffb80b6419d5
LorenLynch <92160547+LorenLynch@users.noreply.github.com>
2022-02-08 08:52:15 +0100

    Indexado

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,35 +1,38 @@
 public class Equipo {
-    private String nombreEquipo;
-    private int ranking;
-        public void setNombreEquipo(String nombreEquipo) {
-                if (nombreEquipo.length() >= 4 && nombreEquipo.length() <= 20) {
-                    for (int i = 0; i < nombreEquipo.length(); i++) {
-                        if (nombreEquipo.toLowerCase().charAt(i) >= 'a' && nombreEquipo.toLowerCase().charAt(i) <= 'z' || nombreEquipo.charAt(i) == ' ') 
-                        {
-                            this.nombreEquipo = nombreEquipo;
-                        } else {
-                            this.nombreEquipo = null;
-                            i = nombreEquipo.length();
-                        }
-                    }
-                } else {
-                    this.nombreEquipo = null;
-                }
-            }
-    public void setRanking(int ranking) {
-        if (ranking >= 0 && ranking <= 10) {
-            this.ranking = ranking;
-        } else {
-            this.ranking = -1;
-        }
-    }
-    public String getNombreEquipo() {
-        return nombreEquipo;
-    }
-    public int getRanking() {
-        return ranking;
-    }
-  public String categoriaEquipo() {
+	private String nombreEquipo;
+	private int ranking;
+	public void setNombreEquipo(String nombreEquipo) {
+		if (nombreEquipo.length() >= 4 && nombreEquipo.length() <= 20) {
+			for (int i = 0; i < nombreEquipo.length(); i++) {
+				if (nombreEquipo.toLowerCase().charAt(i) >= 'a' && nombreEquipo.toLowerCase().charAt(i) <= 'z' || nombreEquipo.charAt(i) == ' ') 
+				{
+					this.nombreEquipo = nombreEquipo;
+				} else {
+					this.nombreEquipo = null;
+					i = nombreEquipo.length();
+				}
+			}
+		} else {
+			this.nombreEquipo = null;
+		}
+	}
+	
+	public void setRanking(int ranking) {
+		if (ranking >= 0 && ranking <= 10) {
+			this.ranking = ranking;
+		} else {
+			this.ranking = -1;
+		}
+	}
+	
+	public String getNombreEquipo() {
+		return nombreEquipo;
+	}
+	
+	public int getRanking() {
+		return ranking;
+	}
+	public String categoriaEquipo() {
 		if(ranking!=-1 && nombreEquipo!=null) {
 			if(ranking>=7 && ranking<=10) {
 				return "Primera";
@@ -44,6 +47,6 @@ public class Equipo {
 				return null;
 			}
 		}else {
-		return null;}
+			return null;}
 	}
 }
