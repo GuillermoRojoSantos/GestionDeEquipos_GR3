30cec8074e4b885b7fd3d7d303a4f6746b5c08af
adrianstrong <93924821+adrianstrong@users.noreply.github.com>
2022-02-09 10:07:25 +0100

    Todas las pruebas correctas
    nombreEquipo no puede ser nulo

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,52 +1,57 @@
 public class Equipo {
 	private String nombreEquipo;
 	private int ranking;
+
 	public void setNombreEquipo(String nombreEquipo) {
-		if (nombreEquipo.length() >= 4 && nombreEquipo.length() <= 20) {
-			for (int i = 0; i < nombreEquipo.length(); i++) {
-				if (nombreEquipo.toLowerCase().charAt(i) >= 'a' && nombreEquipo.toLowerCase().charAt(i) <= 'z' || nombreEquipo.charAt(i) == ' ') 
-				{
-					this.nombreEquipo = nombreEquipo;
-				} else {
-					this.nombreEquipo = null;
-					i = nombreEquipo.length();
+		if (nombreEquipo != null) {
+			if (nombreEquipo.length() >= 4 && nombreEquipo.length() <= 20) {
+				for (int i = 0; i < nombreEquipo.length(); i++) {
+					if (nombreEquipo.toLowerCase().charAt(i) >= 'a' && nombreEquipo.toLowerCase().charAt(i) <= 'z'
+							|| nombreEquipo.charAt(i) == ' ') {
+						this.nombreEquipo = nombreEquipo;
+					} else {
+						this.nombreEquipo = null;
+						i = nombreEquipo.length();
+					}
 				}
+			} else {
+				this.nombreEquipo = null;
 			}
 		} else {
 			this.nombreEquipo = null;
 		}
 	}
-	
+
 	public void setRanking(int ranking) {
+		
 		if (ranking >= 0 && ranking <= 10) {
 			this.ranking = ranking;
 		} else {
 			this.ranking = -1;
 		}
 	}
-	
+
 	public String getNombreEquipo() {
 		return nombreEquipo;
 	}
-	
+
 	public int getRanking() {
 		return ranking;
 	}
+
 	public String categoriaEquipo() {
-		if(ranking!=-1 && nombreEquipo!=null) {
-			if(ranking>=7 && ranking<=10) {
+		if (ranking != -1 && nombreEquipo != null) {
+			if (ranking >= 7 && ranking <= 10) {
 				return "Primera";
-			}
-			else if(ranking>=3 && ranking<=6) {
+			} else if (ranking >= 3 && ranking <= 6) {
 				return "Segunda";
-			}
-			else if(ranking>=0 && ranking<=2) {
+			} else if (ranking >= 0 && ranking <= 2) {
 				return "Tercera";
-			}
-			else {
+			} else {
 				return null;
 			}
-		}else {
-			return null;}
+		} else {
+			return null;
+		}
 	}
 }
