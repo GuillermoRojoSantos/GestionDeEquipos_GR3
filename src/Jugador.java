b4079b8075646ae331df87fb62b90b99896cccfc
josedommarco <92160553+josedommarco@users.noreply.github.com>
2022-02-09 11:04:54 +0100

    Update Jugador.java

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,57 +1,67 @@
-
 public class Jugador {
-	private String nombre;
-	private int edad;
-	private String idioma;
-
-	public Jugador () {
-			
-		}
-
-	public void setNombreJugador(String nombre) {
-		if (nombre != null) {
-			if (nombre.length() < 4 || nombre.length() > 20) {
-				this.nombre = null;
-			} else {
-				this.nombre = nombre;
-			}
-		}else{
-			this.nombre=null;
-		}
-	}
-
-	public void setEdad(int edad) {
-		if (edad != 0) {
-			if (edad < 18) {
-				this.edad = 0;
-			} else {
-				this.edad = edad;
-			}
-		}else {
-			this.edad=0;
-		}
-	}
-	public void setIdioma(String idioma) {
-		if (idioma != null) {
-			if (idioma.equals("Inglés") || idioma.equals("Español") || idioma.equals("Alemán") || idioma.equals("Francés")) {
-				this.idioma = idioma;
-			} else {
-				this.idioma = null;
-			}
-		}else{
-			this.idioma=null;
-		}
-	}
-
-
-	public String getIdioma() {
-		return idioma;
-	}
-	
-	public String getNombreJugador() {
-		return nombre;
-	}
-	public int getEdad() {
-		return edad;
-	}
+    private String nombre;
+    private int edad;
+    private String idioma;
+    public void setNombreJugador(String nombre) {
+        if (nombre != null) {
+            if (nombre.length() >= 4 && nombre.length() <= 20) {
+                for (int i = 0; i < nombre.length(); i++) {
+                    if (nombre.toLowerCase().charAt(i) >= 'a' && nombre.toLowerCase().charAt(i) <= 'z'
+                            || nombre.charAt(i) == ' ') {
+                        this.nombre = nombre;
+                    } else {
+                        this.nombre = null;
+                        i = nombre.length();
+                    }
+                }
+            } else {
+                this.nombre = null;
+            }
+        } else {
+            this.nombre = null;
+        }
+    }
+    public void setEdad(int edad) {
+        if (edad<18){
+            this.edad=0;
+        }else{
+            this.edad=edad;
+        }
+    }
+    public void setIdioma(String idioma) {
+        if(idioma!=null) {
+            if (idioma.equals("Inglés")||idioma.equals("Español")||idioma.equals("Alemán")||idioma.equals("Francés")) {
+                this.idioma=idioma;
+            }else{
+                this.idioma=null;
+            }
+        }
+    }
+    public String getIdioma() {
+        return idioma;
+    }
+    public String getNombreJugador() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public String tipoJugador(int edad,String nombre,String idioma){
+        String tipoJugador="";
+        if(edad!=0 && nombre!=null && idioma!=null) {
+            if(edad>=18 && edad<=25) {
+                tipoJugador="Junior";
+            }
+            else if(edad>=26 && edad<=35) {
+                tipoJugador="Senior";
+            }
+            else {
+                tipoJugador="Master";
+            }
+        }
+        else {
+            tipoJugador=null;
+        }
+        return tipoJugador;
+    }
 }
