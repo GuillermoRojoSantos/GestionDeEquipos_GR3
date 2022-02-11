a26951ab98466f6f84b895a4eda941ed75d91e51
LorenLynch <92160547+LorenLynch@users.noreply.github.com>
2022-02-11 12:29:07 +0100

    condicion mayus

--- src/Equipo.java
+++ src/Equipo.java
@@ -2,11 +2,14 @@ public class Equipo {
 	private String nombreEquipo;
 	private int ranking;
 
+	public Equipo() {
+	}
+
 	public void setNombreEquipo(String nombreEquipo) {
 		if (nombreEquipo != null) {
 			if (nombreEquipo.length() >= 4 && nombreEquipo.length() <= 20) {
 				for (int i = 0; i < nombreEquipo.length(); i++) {
-					if (nombreEquipo.toLowerCase().charAt(i) >= 'a' && nombreEquipo.toLowerCase().charAt(i) <= 'z'
+					if (nombreEquipo.charAt(i) >= 'A' && nombreEquipo.charAt(i) <= 'Z'
 							|| nombreEquipo.charAt(i) == ' ') {
 						this.nombreEquipo = nombreEquipo;
 					} else {
@@ -23,7 +26,7 @@ public class Equipo {
 	}
 
 	public void setRanking(int ranking) {
-		
+
 		if (ranking >= 0 && ranking <= 10) {
 			this.ranking = ranking;
 		} else {
@@ -33,25 +36,11 @@ public class Equipo {
 
 	public String getNombreEquipo() {
 		return nombreEquipo;
+
 	}
 
 	public int getRanking() {
 		return ranking;
-	}
 
-	public String categoriaEquipo() {
-		if (ranking != -1 && nombreEquipo != null) {
-			if (ranking >= 7 && ranking <= 10) {
-				return "Primera";
-			} else if (ranking >= 3 && ranking <= 6) {
-				return "Segunda";
-			} else if (ranking >= 0 && ranking <= 2) {
-				return "Tercera";
-			} else {
-				return null;
-			}
-		} else {
-			return null;
-		}
 	}
 }
