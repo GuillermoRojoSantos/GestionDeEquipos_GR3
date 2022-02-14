60f091b033f01a09df56816681f41ac0ef13a8ff
adrianstrong <93924821+adrianstrong@users.noreply.github.com>
2022-02-14 14:17:44 +0100

    Update AsignarEquipo.java

--- src/AsignarEquipo.java
+++ src/AsignarEquipo.java
@@ -1,88 +1,87 @@
 public class AsignarEquipo {
-      private Equipo equipo;
-      private Jugador jugador;
+	private Equipo equipo;
+	private Jugador jugador;
 
-      public void setEquipo(Equipo equipo) {
-            if (EquipoValido(equipo)) {
-                  if (comprobarCadena(equipo.getNombreEquipo())) {
-                        if (comprobarInt(equipo.getRanking())) {
-                              this.equipo = equipo;
-                        }
-                  }
-            }
-      }
+	public void setEquipo(Equipo equipo) {
+		if (EquipoValido(equipo)) {
+			if (comprobarCadena(equipo.getNombreEquipo())) {
+				if (comprobarInt(equipo.getRanking())) {
+					this.equipo = equipo;
+				}
+			}
+		}
+	}
 
-      public Equipo getEquipo() {
-            return this.equipo;
-      }
+	public Equipo getEquipo() {
+		return this.equipo;
+	}
 
-      public void setJugador(Jugador jugador) {
-            //Comprobamos si es null
-            if (JugadorValido(jugador)) {
-                  if (comprobarCadena(jugador.getNombreJugador())) {
-                        if (comprobarInt(jugador.getEdad())) {
-                              if (comprobarCadena(jugador.getIdioma())) {
-                                    this.jugador = jugador;
-                              }
-                        }
-                  }
-            }
-      }
+	public void setJugador(Jugador jugador) {
+		if (JugadorValido(jugador)) {
+			if (jugador.getNombreJugador()!=null) {
+				if (jugador.getEdad()!=0) {
+					if (comprobarCadena(jugador.getIdioma())) {
+						this.jugador = jugador;
+					}
+				}
+			}
+		} else {
+			this.jugador = null;
+		}
+	}
 
+	public Jugador getJugador() {
+		return this.jugador;
+	}
 
-      public Jugador getJugador() {
-            return this.jugador;
-      }
+	private boolean JugadorValido(Jugador jugador) {
+		boolean valido = true;
 
-      //Nos devuelve un true si es distinto a nulo.
-      private Boolean JugadorValido(Jugador comprobar) {
-            boolean valido = true;
+		if (jugador == null) {
+			valido = false;
+		} else {
+			valido = true;
+		}
 
-            if (comprobar == null) {
-                  valido = false;
+		return valido;
+	}
+
+	private boolean EquipoValido(Equipo equipo) {
+		boolean valido = true;
+
+		if (equipo == null) {
+			valido = false;
+		} else {
+			valido = true;
+		}
+
+		return valido;
+	}
+
+	private boolean comprobarInt(int ranking) {
+		boolean hasRanking = true;
+
+		if (ranking != 0) {
+			hasRanking = true;
+		} else {
+			hasRanking = false;
+		}
+
+		return hasRanking;
+	}
+
+	private boolean comprobarCadena(String name){
+        boolean nombreCorrecto = true;
+
+        if (name != null){
+            if (name.length()>0){
+                nombreCorrecto = true;
             } else {
-                  valido = true;
+                nombreCorrecto = false;
             }
-
-            return valido;
-      }
-
-      private boolean EquipoValido(Equipo comprobar) {
-            boolean valido = true;
-
-            if (comprobar == null) {
-                  valido = false;
-            } else {
-                  valido = true;
-            }
-
-            return valido;
-      }
-
-      private boolean comprobarInt(int rank) {
-            boolean hasRanking = true;
-
-            if (rank != 0) {
-                  hasRanking = true;
-            } else {
-                  hasRanking = false;
-            }
-
-            return hasRanking;
-      }
-
-      private boolean comprobarCadena(String name) {
-            boolean nombreCorrecto = true;
-
-            if (name != null) {
-                  if (name.length() > 0) {
-                        nombreCorrecto = true;
-                  } else {
-                        nombreCorrecto = false;
-                  }
-            } else {
-                  nombreCorrecto = false;
-            }
-            return nombreCorrecto;
-      }
+        } else {
+            nombreCorrecto = false;
+        }
+        return nombreCorrecto;
+    }
 }
