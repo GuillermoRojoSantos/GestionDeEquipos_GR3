ece27716e4d4779bce62f414b667d9374d1f1ac7
LorenLynch <92160547+LorenLynch@users.noreply.github.com>
2022-02-14 14:51:16 +0100

    FINAL

--- src/AsignarEquipo.java
+++ src/AsignarEquipo.java
@@ -1,7 +1,17 @@
+/**
+ * Esta clase guarda un equipo y un jugador si recibe correctos todos los campos 
+ * @author LUIS GUTIÉRREZ CHAMIZO
+ *
+ */
+
 public class AsignarEquipo {
 	private Equipo equipo;
 	private Jugador jugador;
 
+	/**
+	 * Este método guarda un equipo si recibe de los campos con el formato correcto 
+	 * @param equipo que se quiere guardar
+	 */
 	public void setEquipo(Equipo equipo) {
 		if (EquipoValido(equipo)) {
 			if (comprobarCadena(equipo.getNombreEquipo())) {
@@ -12,10 +22,18 @@ public class AsignarEquipo {
 		}
 	}
 
+	/**
+	 * Getter de equipo 
+	 * @return equipo
+	 */
 	public Equipo getEquipo() {
 		return this.equipo;
 	}
 
+	/**
+	 * Método que guarda un jugador si todos sus campos son correctos
+	 * @param jugador que se quiere guardar
+	 */
 	public void setJugador(Jugador jugador) {
 		if (JugadorValido(jugador)) {
 			if (jugador.getNombreJugador()!=null) {
@@ -30,10 +48,19 @@ public class AsignarEquipo {
 		}
 	}
 
+	/**
+	 * Getter de jugador
+	 * @return jugador 
+	 */
 	public Jugador getJugador() {
 		return this.jugador;
 	}
 
+	/**
+	 * 
+	 * @param jugador que se quiere guardar
+	 * @return boolean valido (true si jugador no es null)
+	 */
 	private boolean JugadorValido(Jugador jugador) {
 		boolean valido = true;
 
@@ -46,6 +73,11 @@ public class AsignarEquipo {
 		return valido;
 	}
 
+	/**
+	 * 
+	 * @param equipo que se quiere guardar
+	 * @return boolean valido (true si el equipo no es null)
+	 */
 	private boolean EquipoValido(Equipo equipo) {
 		boolean valido = true;
 
@@ -58,6 +90,10 @@ public class AsignarEquipo {
 		return valido;
 	}
 
+	/**
+	 * @param ranking del equipo que se quiere guardar
+	 * @return boolean hasRanking (true si el ranking es distinto de 0)
+	 */
 	private boolean comprobarInt(int ranking) {
 		boolean hasRanking = true;
 
@@ -70,6 +106,11 @@ public class AsignarEquipo {
 		return hasRanking;
 	}
 
+	/**
+	 * 
+	 * @param name del jugador que se quiere guardar
+	 * @return boolean nombreCorrecto (devuelve true si el nombre es correcto)
+	 */
 	private boolean comprobarCadena(String name){
         boolean nombreCorrecto = true;
 
